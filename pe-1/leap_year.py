f509ed8a7c663a386e0082a092faac301796dbbf
shiojojo <shiroshi309@gmail.com>
2024-08-09 20:35:04 +0900

    total day

--- pe-1/leap_year.py
+++ pe-1/leap_year.py
@@ -1,16 +1,14 @@
 def is_year_leap(year):
-#
-# Your code from LAB 4.3.1.6.
-#
+    if year < 1582:
+        return None
     if year % 4 != 0 or (year % 100 == 0 and year % 400 != 0):
         return False
     else:
         return True
 
 def days_in_month(year, month):
-#
-# Write your new code here.
-#
+    if (year == 1582 and month < 10) or is_year_leap(year) == None:
+        return None
     if month == 2:
         if is_year_leap(year):
             return 29
@@ -18,19 +16,20 @@ def days_in_month(year, month):
             return 28
     elif month in [4,6,9,11]:
         return 30
-    else:
+    elif month in [1,3,5,7,8,9,10,12]:
         return 31
+    else:
+        return None
 
+def day_of_year(year, month, day):
+    if year == 1582:
+        return None
+        
+    check_days = days_in_month(year, month)
+    if day <= 0 or day > check_days or check_days == None:
+        return None
+    for i in range(1,month):
+        day += days_in_month(year, i)     
+    return day
 
-test_years = [1900, 2000, 2016, 1987]
-test_months = [2, 2, 1, 11]
-test_results = [28, 29, 31, 30]
-for i in range(len(test_years)):
-	yr = test_years[i]
-	mo = test_months[i]
-	print(yr, mo, "->", end="")
-	result = days_in_month(yr, mo)
-	if result == test_results[i]:
-		print("OK")
-	else:
-		print("Failed")
+print(day_of_year(1582, 12, 21))
