1b86afd3d7c96d4c18a7c57c27a0733f37fca994
shiojojo <shiroshi309@gmail.com>
2024-08-17 18:47:53 +0900

    change public var to private

--- pe-2/points_plane.py
+++ pe-2/points_plane.py
@@ -3,23 +3,22 @@ import math
 
 class Point:
     def __init__(self, x=0.0, y=0.0):
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
 
     def getx(self):
-        return self.x
+        return self.__x
 
     def gety(self):
-        return self.y
+        return self.__y
 
     def distance_from_xy(self, x, y):
-        distance = ((self.x - x) ** 2 + (self.y - y) ** 2) ** 0.5
+        distance = ((self.__x - x) ** 2 + (self.__y - y) ** 2) ** 0.5
         return distance
 
     def distance_from_point(self, point):
-        distance = ((self.x - point.getx()) ** 2 + (self.y - point.gety()) ** 2) ** 0.5
+        distance = ((self.__x - point.getx()) ** 2 + (self.__y - point.gety()) ** 2) ** 0.5
         return distance
-
 
 point1 = Point(0, 0)
 point2 = Point(1, 1)
